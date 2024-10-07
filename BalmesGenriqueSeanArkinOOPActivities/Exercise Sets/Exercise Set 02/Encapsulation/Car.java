3a321cd502aed9523a7839d6a80bb0b6adb4ba7f
GenriqueBalmes <163899643+GenriqueBalmes@users.noreply.github.com>
2024-10-07 10:27:17 +0800

    Add files via upload

---
+++ BalmesGenriqueSeanArkinOOPActivities/Exercise Sets/Exercise Set 02/Encapsulation/Car.java
@@ -0,0 +1,64 @@
+public class Car {
+    private String color;
+    private double price;
+    private char size;
+
+    public Car(String color, double price, char size) {
+        this.color = color;
+        this.price = price;
+        this.size = Character.toUpperCase(size);
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public char getSize() {
+        return size;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setSize(char size) {
+        this.size = Character.toUpperCase(size);
+    }
+
+    public String toString() {
+        String sizeDescription;
+        switch (size) {
+            case 'S':
+                sizeDescription = "small";
+                break;
+            case 'M':
+                sizeDescription = "medium";
+                break;
+            case 'L':
+                sizeDescription = "large";
+                break;
+            default:
+                sizeDescription = "unknown";
+        }
+
+        return "Car (" + color + ") - P" + String.format("%.2f", price) + " - " + sizeDescription;
+    }
+
+    public static void main(String[] args) {
+        Car car1 = new Car("black", 86000.86, 'S');  
+        Car car2 = new Car("white", 139000.21, 'M'); 
+        Car car3 = new Car("green", 249456.99, 'L'); 
+
+        System.out.println(car1);
+        System.out.println(car2);
+        System.out.println(car3);
+    }
+}
